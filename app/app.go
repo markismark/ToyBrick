cea39df984fde96e07d6fc27ba3484919c2e17db
maxin <maxinvc@126.com>
2016-04-13 22:21:53 +0800

    add sendRequest

--- app/app.go
+++ app/app.go
@@ -1,18 +1,31 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func Run() {
-	http.Handle("/", resquestHandler)
-	http.ListenAndServe("8123", nil)
+	//	http.HandleFunc("/", resquestHandler)
+	//	err := http.ListenAndServe(":8123", nil)
+	//	if err != nil {
+	//		log.Fatal("ListenAndServe:", err)
+	//	}
+	//	fmt.Println("begin run")
+	tr := &TextRequest{Method: "post", Url: "http://buy.mi.com/in/accessories"}
+	hr, _ := BuildHttpRequest(tr)
+	response, _ := SendRequest(hr)
+	fmt.Println(response)
 }
 
 func resquestHandler(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Fprintf(w, "Hello ! welcome")
 }
 
 func loadConfig() {
+
+}
+
+func parseParams() {
 
 }
--- app/request.go
+++ app/request.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -16,6 +18,22 @@ type TextRequest struct {
 	Timeout time.Duration
 }
 
-func BuildHttpRequest(tr *TextRequest) *http.Request {
+func BuildHttpRequest(tr *TextRequest) (*http.Request, error) {
+	req, err := http.NewRequest(tr.Method, tr.Url, bytes.NewBuffer([]byte{}))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "text/plain")
 
+	return req, nil
+}
+
+func SendRequest(hr *http.Request) (*TextResponse, error) {
+	client := &http.Client{}
+	resp, _ := client.Do(hr)
+	body, _ := ioutil.ReadAll(resp.Body)
+	tr := &TextResponse{}
+	tr.Body = string(body)
+	tr.HttpStatus = resp.StatusCode
+	return tr, nil
 }
