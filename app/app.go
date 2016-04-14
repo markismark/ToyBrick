b4616a3e31df7da49e6ad0b84e0c755d5ca33213
maxin <maxinvc@126.com>
2016-04-14 13:58:44 +0800

    add parse param and return data

--- app/app.go
+++ app/app.go
@@ -2,24 +2,38 @@ package app
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func Run() {
-	//	http.HandleFunc("/", resquestHandler)
-	//	err := http.ListenAndServe(":8123", nil)
-	//	if err != nil {
-	//		log.Fatal("ListenAndServe:", err)
-	//	}
-	//	fmt.Println("begin run")
-	tr := &TextRequest{Method: "post", Url: "http://buy.mi.com/in/accessories"}
-	hr, _ := BuildHttpRequest(tr)
-	response, _ := SendRequest(hr)
-	fmt.Println(response)
+	http.HandleFunc("/", resquestHandler)
+	err := http.ListenAndServe(":8123", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+	fmt.Println("begin run")
+
 }
 
 func resquestHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello ! welcome")
+	if r.URL.Path == "/favicon.ico" {
+		return
+	}
+	r.ParseForm()
+	data := r.Form["data"]
+	if len(data) == 0 {
+		return
+	}
+	textRequestJson := data[0]
+	tr := createTextRequest(textRequestJson, r)
+	hr, _ := BuildHttpRequest(tr)
+	response, err := SendRequest(hr)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	fmt.Fprintf(w, response.Body)
 }
 
 func loadConfig() {
--- app/request.go
+++ app/request.go
@@ -1,37 +1,81 @@
 package app
 
 import (
-	"bytes"
+	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"time"
+	"strings"
 )
 
 type TextRequest struct {
-	Header  map[string]string
-	Method  string
-	Cookie  map[string]string
-	Data    map[string]map[string]string
-	Url     string
-	Status  string
-	Retry   int
-	Timeout time.Duration
+	Header      map[string]string            `json:header`
+	Method      string                       `json:method`
+	Cookies     map[string]string            `json:cookies`
+	Data        map[string]map[string]string `json:data`
+	Url         string                       `json:url`
+	Status      string                       `json:-`
+	Retry       int                          `json:retry`
+	Timeout     int                          `json:timeout`
+	UserRequset *http.Request                `json:-`
+}
+
+var (
+	UserHeader = []string{
+		"Content-Type",
+		"Accept",
+		"Accept-Language",
+		"User-Agent",
+		"Referer",
+		"Cache-Control",
+		"Cookie",
+		"If-Modified-Since",
+		"Etag",
+	}
+)
+
+func createTextRequest(jsonStr string, hr *http.Request) *TextRequest {
+	tr := &TextRequest{}
+	json.Unmarshal([]byte(jsonStr), tr)
+	tr.Method = strings.ToUpper(tr.Method)
+	tr.UserRequset = hr
+	return tr
 }
 
 func BuildHttpRequest(tr *TextRequest) (*http.Request, error) {
-	req, err := http.NewRequest(tr.Method, tr.Url, bytes.NewBuffer([]byte{}))
+	req, err := http.NewRequest(tr.Method, tr.Url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "text/plain")
 
+	for _, key := range UserHeader {
+		if value := tr.UserRequset.Header.Get(key); value != "" {
+			req.Header.Set(key, value)
+		}
+	}
 	return req, nil
 }
 
 func SendRequest(hr *http.Request) (*TextResponse, error) {
 	client := &http.Client{}
-	resp, _ := client.Do(hr)
-	body, _ := ioutil.ReadAll(resp.Body)
+	resp, perr := client.Do(hr)
+
+	if perr != nil {
+		return nil, perr
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+			return
+		} else {
+			resp.Body.Close()
+		}
+	}()
+	log.Println(resp)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 	tr := &TextResponse{}
 	tr.Body = string(body)
 	tr.HttpStatus = resp.StatusCode
