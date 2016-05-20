278b1b8e803132683a80b794db458da0060c999a
maxin <maxinvc@126.com>
2016-05-20 10:05:04 +0800

    init group

--- conf/group.go
+++ conf/group.go
@@ -7,8 +7,12 @@ import (
 	"github.com/go-ini/ini"
 )
 
+var (
+	Tags    = make(map[string]*Group)
+	TIMEOUT = 10000
+)
+
 type Group struct {
-	Name     string
 	Timeout  int
 	Protocol string
 	Balance  string
@@ -29,8 +33,44 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	tags := cfg.Sections()
-	for _, tag := range tags {
-		log.Printf("%#v", tag.Name())
+	sections := cfg.Sections()
+	for _, section := range sections {
+		Tags[section.Name()] = initGroup(section)
+		log.Printf("%#v\n", Tags[section.Name()])
 	}
+
+	//log.Printf("%#v\n", Tags)
+}
+
+func initGroup(section *ini.Section) *Group {
+	timeout := getTimeout(section)
+	protocol := getProtocol(section)
+	return &Group{Timeout: timeout, Protocol: protocol}
+
+}
+
+func getTimeout(section *ini.Section) int {
+	var err error
+	var timeout int
+	var timeoutKey *ini.Key
+	timeoutKey, err = section.GetKey("timeout")
+	if err != nil {
+		timeout = TIMEOUT
+	} else {
+		timeout, err = timeoutKey.Int()
+		if err != nil {
+			timeout = TIMEOUT
+		}
+	}
+	return timeout
+}
+
+func getProtocol(section *ini.Section) string {
+	var err error
+	var protocolKey *ini.Key
+	protocolKey, err = section.GetKey("protocol")
+	if err != nil {
+		return ""
+	}
+	return protocolKey.String()
 }
