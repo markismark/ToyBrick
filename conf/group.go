65ff3bc1196e52f61d7d310576e2594b6ee39885
maxin <maxinvc@126.com>
2016-05-23 19:50:14 +0800

    add conf

--- conf/group.go
+++ conf/group.go
@@ -8,15 +8,17 @@ import (
 )
 
 var (
-	Tags    = make(map[string]*Group)
-	TIMEOUT = 10000
+	Tags                    = make(map[string]*Group)
+	DEFAULE_TIMEOUT  uint64 = 10000
+	DEFAULT_PROTOCOL        = "http"
+	DEFAULT_MAXRETRY uint   = 1
 )
 
 type Group struct {
-	Timeout  int
+	Timeout  uint64
 	Protocol string
 	Balance  string
-	MaxRetry int
+	MaxRetry uint
 	Host     string
 	Machines *[]Machine
 }
@@ -45,24 +47,25 @@ func init() {
 func initGroup(section *ini.Section) *Group {
 	timeout := getTimeout(section)
 	protocol := getProtocol(section)
-	return &Group{Timeout: timeout, Protocol: protocol}
+	maxRetry := getMaxRetry(section)
+	host := getHost(section)
+	return &Group{
+		Timeout:  timeout,
+		Protocol: protocol,
+		MaxRetry: maxRetry,
+		Host:     host,
+	}
 
 }
 
-func getTimeout(section *ini.Section) int {
+func getTimeout(section *ini.Section) uint64 {
 	var err error
-	var timeout int
 	var timeoutKey *ini.Key
 	timeoutKey, err = section.GetKey("timeout")
 	if err != nil {
-		timeout = TIMEOUT
-	} else {
-		timeout, err = timeoutKey.Int()
-		if err != nil {
-			timeout = TIMEOUT
-		}
+		return DEFAULE_TIMEOUT
 	}
-	return timeout
+	return timeoutKey.MustUint64(DEFAULE_TIMEOUT)
 }
 
 func getProtocol(section *ini.Section) string {
@@ -70,7 +73,32 @@ func getProtocol(section *ini.Section) string {
 	var protocolKey *ini.Key
 	protocolKey, err = section.GetKey("protocol")
 	if err != nil {
+		return DEFAULT_PROTOCOL
+	}
+	protocol := protocolKey.String()
+	if protocol == "" {
+		return DEFAULT_PROTOCOL
+	}
+	return protocol
+}
+
+func getMaxRetry(section *ini.Section) uint {
+	var err error
+	var maxRetryKey *ini.Key
+	maxRetryKey, err = section.GetKey("max_retry")
+	if err != nil {
+		return DEFAULT_MAXRETRY
+	}
+	maxRetry := maxRetryKey.MustUint(DEFAULT_MAXRETRY)
+	return maxRetry
+}
+
+func getHost(section *ini.Section) string {
+	var err error
+	var hostKey *ini.Key
+	hostKey, err = section.GetKey("host")
+	if err != nil {
 		return ""
 	}
-	return protocolKey.String()
+	return hostKey.MustString("")
 }
